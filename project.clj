a9a4953e51a0bf54679db4a4912a8b79d74e7672
Marcelina Hołub <mholub@tutanota.com>
2024-09-01 17:10:48 +0200

    fix: calculating challenge expiration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.mjholub/altcha-clj "1.0.2"
+(defproject me.mjholub/altcha-clj "1.1.0"
   :description "A Clojure/script library designed for working with Altcha challenges."
   :url "https://github.com/mjholub/altcha-clj"
   :license {:name "LGPL-3.0-or-later"
