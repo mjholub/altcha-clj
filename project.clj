378de3595418a23d24ad20f403d980079abd2d78
Marcelina Hołub <mholub@tutanota.com>
2025-12-19 15:34:59 +0100

    chore: bump cheshire

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject me.mjholub/altcha-clj "1.4.0"
+(defproject me.mjholub/altcha-clj "2.0.1"
   :description "A Clojure/script library designed for working with Altcha challenges."
   :url "https://github.com/mjholub/altcha-clj"
   :license {:name "LGPL-3.0-or-later"
             :url "https://www.gnu.org/licenses/lgpl-3.0.html"}
-  :dependencies [[org.clojure/clojure "1.11.2"]
-                 [cheshire "5.13.0"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [cheshire "6.1.0"]
                  [org.bouncycastle/bcprov-ext-jdk18on "1.78.1"]
-                 [pandect "1.0.2"]
-                 ]
+                 [pandect "1.0.2"]]
   :repl-options {:init-ns altcha-clj.core})
